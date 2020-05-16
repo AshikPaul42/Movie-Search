438aa7afa97d04e9ddf5e9b946a930bcd89cf8c8
Ashik Paul <ashik.paul@sacha.solutions>
2020-05-16 15:01:20 +0530

    Updates

---
+++ precache-manifest.dd17a684134ce66b0d5530bd2bb39dcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b092ba02ec6d8fd823326b64cd770fe2",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "1ea95a6e5ac351729879",
+    "url": "/movie-search/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef65421269ec4eca0520",
+    "url": "/movie-search/static/css/main.65a663a5.chunk.css"
+  },
+  {
+    "revision": "1ea95a6e5ac351729879",
+    "url": "/movie-search/static/js/2.c6f0fe5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.c6f0fe5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef65421269ec4eca0520",
+    "url": "/movie-search/static/js/main.7cff6870.chunk.js"
+  },
+  {
+    "revision": "deb853d98ffa0fb9c71b",
+    "url": "/movie-search/static/js/runtime-main.18c73c9f.js"
+  }
+]);
