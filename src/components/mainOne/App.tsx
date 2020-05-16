3ea8cb8524c418d0422cf3cae02b733a3f5d0757
AshikPaul42 <64515863+AshikPaul42@users.noreply.github.com>
2020-05-16 15:07:00 +0530

    removing relative link to bootstap

--- src/components/mainOne/App.tsx
+++ src/components/mainOne/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HeaderSection from '../headerSection/headerSection';
 import SearchSection from '../searchSection/searchSection'
